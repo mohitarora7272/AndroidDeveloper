7f7da44b502a2647b7e05bd239ac99bd64d2d73c
sumeena <sumeena@ourdesignz.com>
2017-06-29 10:20:44 +0530

    Android Development Training Guide.

--- app/src/main/java/com/androiddeveloper/HomeActivity.java
+++ app/src/main/java/com/androiddeveloper/HomeActivity.java
@@ -1,18 +1,18 @@
 package com.androiddeveloper;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.google.android.gms.ads.AdListener;
@@ -20,15 +20,12 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 
-import im.delight.android.webview.AdvancedWebView;
-
 /**
  * Created by @mohit on 13/6/17.
  */
-
 @SuppressWarnings("ALL")
-public class HomeActivity extends AppCompatActivity implements AdvancedWebView.Listener {
-    private AdvancedWebView mWebView;
+public class HomeActivity extends AppCompatActivity {
+    private WebView mWebView;
     private Button btn_Internet;
     private AdView mAdView;
     private InterstitialAd mInterstitialAd;
@@ -42,34 +39,42 @@ public class HomeActivity extends AppCompatActivity implements AdvancedWebView.L
             actionBar.hide();
         }
 
-        mWebView = (AdvancedWebView) findViewById(R.id.webview);
+        mWebView = (WebView) findViewById(R.id.webview);
         btn_Internet = (Button) findViewById(R.id.btn_Internet);
-        mWebView.setListener(this, this);
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String urlNewString) {
+                webView.loadUrl(urlNewString);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                showInterstitial();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+        });
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+        webSettings.enableSmoothTransition();
         mWebView.loadUrl(getString(R.string.url));
-        mWebView.getSettings().setSupportMultipleWindows(true);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         mWebView.setHorizontalScrollBarEnabled(false);
         mWebView.setScrollbarFadingEnabled(true);
         mWebView.setVerticalScrollBarEnabled(false);
-        mWebView.setDesktopMode(true);
-        // disable third-party cookies only
-        mWebView.setThirdPartyCookiesEnabled(false);
-        // or disable cookies in general
-        mWebView.setCookiesEnabled(false);
-
-        mWebView.setWebChromeClient(new WebChromeClient() {
-
-            @Override
-            public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
-                AdvancedWebView newWebView = new AdvancedWebView(HomeActivity.this);
-                // myParentLayout.addView(newWebView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-                WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
-                transport.setWebView(newWebView);
-                resultMsg.sendToTarget();
-                return true;
-            }
-        });
 
         showBannerAd();
         initInterstitialAd();
@@ -87,27 +92,28 @@ public class HomeActivity extends AppCompatActivity implements AdvancedWebView.L
         mAdView.loadAd(new AdRequest.Builder().build());
         mAdView.setAdListener(new AdListener() {
             public void onAdLoaded() {
-                Log.e("Banner","onAdLoaded");
+                Log.e("Banner", "onAdLoaded");
             }
 
             @Override
             public void onAdClosed() {
-                Log.e("Banner","onAdClosed");
+                Log.e("Banner", "onAdClosed");
             }
 
             @Override
             public void onAdFailedToLoad(int errorCode) {
-                Log.e("Banner","onAdFailedToLoad>>"+errorCode);
+                Log.e("Banner", "onAdFailedToLoad>>" + errorCode);
             }
 
             @Override
             public void onAdLeftApplication() {
-                Log.e("Banner","onAdLeftApplication");
+                Log.e("Banner", "onAdLeftApplication");
             }
 
             @Override
             public void onAdOpened() {
-                Log.e("Banner","onAdOpened");
+                Log.e("Banner", "onAdOpened");
+
             }
         });
     }
@@ -121,48 +127,52 @@ public class HomeActivity extends AppCompatActivity implements AdvancedWebView.L
     }
 
     // Show Interstitial Ads
-    private void showInterstitial() {
-        // Show Ads
-        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
-        } else {
-            loadInterstitialAds();
+    private void loadInterstitialAds() {
+        // Request a new ad if one isn't already loaded, hide the button, and kick off the timer.
+        if (!mInterstitialAd.isLoading() && !mInterstitialAd.isLoaded()) {
+            mInterstitialAd.loadAd(new AdRequest.Builder().build());
         }
 
         mInterstitialAd.setAdListener(new AdListener() {
             public void onAdLoaded() {
-                Log.e("Interstitial","onAdLoaded");
+                Log.e("Interstitial", "onAdLoaded");
             }
 
             @Override
             public void onAdClosed() {
-                Log.e("Interstitial","onAdClosed");
+                Log.e("Interstitial", "onAdClosed");
                 // Load the next interstitial.
                 loadInterstitialAds();
             }
 
             @Override
             public void onAdFailedToLoad(int errorCode) {
-                Log.e("Interstitial","onAdFailedToLoad>>"+errorCode);
+                Log.e("Interstitial", "onAdFailedToLoad>>" + errorCode);
             }
 
             @Override
             public void onAdLeftApplication() {
-                Log.e("Interstitial","onAdLeftApplication");
+                Log.e("Interstitial", "onAdLeftApplication");
             }
 
             @Override
             public void onAdOpened() {
-                Log.e("Interstitial","onAdOpened");
+                Log.e("Interstitial", "onAdOpened");
             }
         });
     }
 
-    private void loadInterstitialAds() {
-        // Request a new ad if one isn't already loaded, hide the button, and kick off the timer.
-        if (!mInterstitialAd.isLoading() && !mInterstitialAd.isLoaded()) {
-            mInterstitialAd.loadAd(new AdRequest.Builder().build());
-        }
+    // Show Interstitial Ad
+    private void showInterstitial() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // Show Ads
+                if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }
+            }
+        });
     }
 
     @SuppressLint("NewApi")
@@ -192,54 +202,38 @@ public class HomeActivity extends AppCompatActivity implements AdvancedWebView.L
         if (mAdView != null) {
             mAdView.pause();
         }
+
         super.onPause();
     }
 
+    @SuppressLint("NewApi")
     @Override
     protected void onDestroy() {
-        mWebView.onDestroy();
         if (mAdView != null) {
             mAdView.destroy();
         }
+
         if (mInterstitialAd != null) {
             mInterstitialAd = null;
         }
+
         super.onDestroy();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        mWebView.onActivityResult(requestCode, resultCode, intent);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (!mWebView.onBackPressed()) {
-            return;
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
         }
-        super.onBackPressed();
-    }
-
-    @Override
-    public void onPageStarted(String url, Bitmap favicon) {
-    }
-
-    @Override
-    public void onPageFinished(String url) {
-        showInterstitial();
-    }
-
-    @Override
-    public void onPageError(int errorCode, String description, String failingUrl) {
-    }
-
-    @Override
-    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
-    }
-
-    @Override
-    public void onExternalPageRequest(String url) {
+        return super.onKeyDown(keyCode, event);
     }
 
     // Check Internet Available Or Not
